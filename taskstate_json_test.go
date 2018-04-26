16163be8ba44db236b633d261cb893e2a1d409c2
Javier Escalada Gómez <kerrigan29a@kerriganBookPro.local>
2018-04-26 15:48:50 +0200

    Tests refactorization

--- taskstate_json_test.go
+++ taskstate_json_test.go
@@ -6,44 +6,28 @@ import (
 	"testing"
 )
 
-func _test(original TaskState) bool {
-	b := bytes.Buffer{}
-	e := json.NewEncoder(&b)
-	err := e.Encode(original)
-	if err != nil {
-		panic(err)
-	}
-	var obtained TaskState
-	err = json.NewDecoder(&b).Decode(&obtained)
-	if err != nil {
-		panic(err)
-	}
+func TestTaskState_EncodeAndDecode(t *testing.T) {
+	_testEncodeAndDecode := func(t *testing.T, original TaskState) {
+		b := bytes.Buffer{}
+		e := json.NewEncoder(&b)
+		err := e.Encode(original)
+		if err != nil {
+			panic(err)
+		}
+		var obtained TaskState
+		err = json.NewDecoder(&b).Decode(&obtained)
+		if err != nil {
+			panic(err)
+		}
 
-	return original == obtained
-}
-
-func TestTaskState_JSON(t *testing.T) {
-	if !_test(Disabled) {
-		t.Errorf("Error testing %s", Disabled)
+		if original != obtained {
+			t.Errorf("Error testing %s", original)
+		}
 	}
-
-	if !_test(Enabled) {
-		t.Errorf("Error testing %s", Enabled)
-	}
-
-	if !_test(Waiting) {
-		t.Errorf("Error testing %s", Waiting)
-	}
-
-	if !_test(Running) {
-		t.Errorf("Error testing %s", Running)
-	}
-
-	if !_test(Success) {
-		t.Errorf("Error testing %s", Success)
-	}
-
-	if !_test(Failure) {
-		t.Errorf("Error testing %s", Failure)
-	}
+	_testEncodeAndDecode(t, Disabled)
+	_testEncodeAndDecode(t, Enabled)
+	_testEncodeAndDecode(t, Waiting)
+	_testEncodeAndDecode(t, Running)
+	_testEncodeAndDecode(t, Success)
+	_testEncodeAndDecode(t, Failure)
 }
